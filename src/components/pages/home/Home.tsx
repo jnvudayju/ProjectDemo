59a1f1baa73aef2b95071be5d3cfaa1891a5ad29
Madan Gopal <madan.23.2@mountblue.tech>
2023-10-06 00:20:55 +0530

    Created Home Page

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,5 +1,21 @@
+import "./style.scss";
 const Home = () => {
-  return <>Home</>;
+  return (<>
+    <div className="frame">
+      <div className="content">
+        <div className="first">
+          <h1>Welcome </h1>
+        </div>
+        <div className="second">
+          <h1>Welcome to CryptogradAI</h1>
+          <h5>Empowering the crypto community with expert-curated crypto knowledge</h5>
+          <button className ="button-63">Get Started</button>
+          
+        </div>
+      </div>
+
+    </div>
+  </>);
 };
 
 export default Home;
